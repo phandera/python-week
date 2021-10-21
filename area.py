43de563e49290af61551f4e91c2762a8c4b9235c
Michael Phandera <michael.phandera@undp.org>
2021-10-21 12:35:34 +0200

    added imports work

---
+++ area.py
@@ -0,0 +1,28 @@
+""" area module written by michael """
+from math import pi 
+def square(a):
+    """ square area function """
+    #square
+    print(a*a)
+    
+def circle(r):
+    """ circle area function """
+    #circle
+    print(pi*r*r)
+    
+def triangle(b,h):
+    """ triangle area function """
+    #traingle
+    print(1/2*b*h)
+
+def trapezium(a,b,h):
+    """ trapezium area function """
+    #trapezium
+    print(1/2*(a+b)*h)
+    
+def rectangle(l,w):
+    """ rectangle area function """
+    #rectangle
+    print(l*w)
+
+
---
+++ greeting.py
@@ -0,0 +1,8 @@
+def mwadzuka(name):
+    """ Morning in chichewa ia called Mwadzuka """
+    print("Mwadzuka bwanji "+name)
+    
+def mwaswera(name):
+    """ afternoon in chichewa ia called Mwadzuka """
+    print("Afternoon bwanji "+name)
+
